3581744668be075d56cf05fff0376700590795a0
Kat B <katreesebarnes@gmail.com>
2019-06-02 19:20:29 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -5,7 +5,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
      t.string :location
      t.string :theme
      t.integer :price
-     t
+     t.boolean :family_friendly
   end 
  end
 end
