f4af155596330e316efdf90cfe0d3c2f2eb568f7
Kat B <katreesebarnes@gmail.com>
2019-06-02 19:18:33 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 3) do
   create_table "haunted_houses", force: :cascade do |t|
     t.string "name"
     t.string "location"
+    t.string "theme"
   end
 
 end
